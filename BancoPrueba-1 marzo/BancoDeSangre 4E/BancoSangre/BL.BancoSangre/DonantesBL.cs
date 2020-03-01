8d6da0bb077ef7b8766cd9b93b516e9179cb3b5c
kevinferrufino <kevsu3003@gmail.com>
2020-03-01 17:35:59 -0600

    Conectando a la base de datos

---
+++ BancoPrueba-1 marzo/BancoDeSangre 4E/BancoSangre/BL.BancoSangre/DonantesBL.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.BancoSangre
+{
+    public class DonantesBL
+    {
+        Contexto _contexto;
+        public BindingList<Donantes> ListaDonantes { get; set; }
+
+        public DonantesBL()
+        {
+            _contexto = new Contexto();
+            ListaDonantes = new BindingList<Donantes>();
+
+          
+        }
+
+        public BindingList<Donantes> ObtenerDonantes()
+        {
+            _contexto.Donantes.Load();
+            ListaDonantes = _contexto.Donantes.Local.ToBindingList();
+            return ListaDonantes;
+        }
+        public Resultado GuardarDonante(Donantes donante)
+        {
+            var resultado = Validar(donante);
+            if (resultado.Exitoso == false)
+            {
+                return resultado;
+            }
+
+            _contexto.SaveChanges();
+
+
+            resultado.Exitoso = true;
+            return resultado;
+        }
+
+        public void AgregarDonante()
+        {
+            var nuevoDonante = new Donantes();
+            _contexto.Donantes.Add(nuevoDonante);
+        }
+
+        public bool EliminarDonante(int id)
+        {
+            foreach (var donante in ListaDonantes)
+            {
+                if (donante.Id == id)
+                {
+                    ListaDonantes.Remove(donante);
+                    _contexto.SaveChanges();
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private Resultado Validar(Donantes donante)
+        {
+            var resultado = new Resultado();
+            resultado.Exitoso = true;
+
+            if (string.IsNullOrEmpty(donante.Nombre) == true)
+            {
+                resultado.Mensaje = "Ingrese el Nombre del Donante";
+                resultado.Exitoso = false;
+            }
+
+            else
+            {
+                if (string.IsNullOrEmpty(donante.Identidad) == true)
+                {
+                    resultado.Mensaje = "Ingrese un número de identidad válido";
+                    resultado.Exitoso = false;
+                }
+                else
+                {
+                    if (string.IsNullOrEmpty(donante.Telefono) == true)
+                    {
+                        resultado.Mensaje = "Ingrese un número de teléfono válido";
+                        resultado.Exitoso = false;
+                    }
+                    else
+                    {
+                        if (string.IsNullOrEmpty(donante.Direccion) == true)
+                        {
+                            resultado.Mensaje = "Ingrese una dirección válida";
+                            resultado.Exitoso = false;
+                        }
+                        else
+                        {
+                            if (string.IsNullOrEmpty(donante.Fecha) == true)
+                            {
+                                resultado.Mensaje = "Ingrese una fecha válida";
+                                resultado.Exitoso = false;
+                            }
+                        }
+                    }
+                }
+      
+            }
+
+            
+            
+
+            return resultado;
+        }
+
+    }
+
+    public class Donantes
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Identidad { get; set; }
+        public string Fecha { get; set; }
+        public string Telefono { get; set; }
+        public string Direccion { get; set; }
+    }
+    public class Resultado
+    {
+        public bool Exitoso { get; set; }
+        public string Mensaje { get; set; }
+    }
+
+}
